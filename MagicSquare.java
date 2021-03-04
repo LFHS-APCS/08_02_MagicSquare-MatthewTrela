dcca17fab9d0b92f02f268f9e61b9875b3a4be32
MatthewTrela <69909901+MatthewTrela@users.noreply.github.com>
2021-03-03 19:18:48 -0600

    Update MagicSquare.java

--- MagicSquare.java
+++ MagicSquare.java
@@ -25,9 +25,56 @@ public class MagicSquare
      */
     public boolean isMagicSquare()
     {
-        return false;   // complete this method
+      if (!isSquare()) {
+        return false;
+      }
+      int mNum = getMagicNum();
+      int total = 0;
+      for (int i = 0; i < array.length; i++) {
+        for (int j = 0; j < array[i].length; j++) {
+          total += array[i][j];
+        }
+        if (total != mNum) {
+          return false;
+        }
+        total = 0;
+      }
+      for (int n = 0; n < array.length; n++) {
+        for (int m = 0; m < array.length; m++) {
+          total += array[m][n];
+        }
+        if (total != mNum) {
+          return false;
+        }
+        total = 0;
+      }
+      for (int p = 0; p < array.length; p++) {
+        total += array[p][p];
+      }
+      if (total != mNum) {
+        return false;
+      }
+      total = 0;
+        return true;   // complete this method
     }
 
+    private int getMagicNum() {
+      int magicNum = 0;
+      for (int i = 0; i < array.length; i++) {
+        magicNum += array[0][i];
+      }
+      return magicNum;
+    }
+
+    private boolean isSquare() {
+      int height = array.length;
+      for (int s = 0; s < array.length; s++) {
+        if (array[s].length != height) {
+          return false;
+        }
+      }
+      return true;
+    }
     /**
      * Returns a String which represents the array.
      */
